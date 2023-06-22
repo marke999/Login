e473c63300e201e6b410d71e1f4e6cda1c2e50d6
lgucebu1 <lgucebu1@lgucebu1@ramesessystems.org>
2023-06-22 14:59:07 +0800

    second commit

--- emaillog.sh
+++ emaillog.sh
@@ -4,7 +4,7 @@
 cd /home/lgucebu1/Tasks/Login
 
 #Path to login3.sh script
-script_path="/home/lgucebu1/Tasks/Login/login3.sh"
+#script_path="/home/lgucebu1/Tasks/Login/login3.sh"
 script_path="/home/lgucebu1/Tasks/Login/login2.sh"
 
 #Run the script using the source command
